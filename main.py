a2b9801db41ebeb479f5fc766c3a62358634a4e2
ITZEL CORTES AGUIRRE <1724110595@utectulancingo.edu.mx>
2025-08-02 03:13:35 +0000

    tengo sueño

--- main.py
+++ main.py
@@ -1057,3 +1057,4 @@ if __name__ == "__main__":
 # Estoy sola programando porque no se como hacerlo y entendiendo de todo para no ser una carga
 # y cada dia me siento peor, casi me dejan mas de 4 veces, solo quiero saber querer...
 # y que me quieran, no quiero ser una carga, no quiero que me dejen, no quiero estar sola
+# Quizás solo me estoy victimizando, solo no tengo donde hablarlo o con quien hablarlo
